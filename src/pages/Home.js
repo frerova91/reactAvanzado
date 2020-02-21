e647b7dbfe72bf8f69cb4a93ee029a11dc886810
unreal-kubuntu <frerova91@gmail.com>
2020-02-21 14:24:26 -0400

    Añadiendo helmet para la App

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,16 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { ListOfCategories } from "../components/ListOfCategories";
 import { ListOfPhotoCards } from "../components/ListOfPhotoCards";
+import { Layout } from "../components/Layout";
 
-export const Home = ({ id }) => {
+export const Home = ({ categoryId }) => {
   return (
-    <Fragment>
+    <Layout
+      title="Tu app de fotos de mascotas"
+      subtitle="Con Petgram puedes encontrar fotos de animales domésticos muy bonitos"
+    >
       <ListOfCategories />
-      <ListOfPhotoCards categoryId={id} />
-    </Fragment>
+      <ListOfPhotoCards categoryId={categoryId} />
+    </Layout>
   );
 };
