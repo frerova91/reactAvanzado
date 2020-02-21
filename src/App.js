b69969721ece7a9497045bf0f5c92edabff02596
unreal-kubuntu <frerova91@gmail.com>
2020-02-21 11:58:21 -0400

    Testing Routes fix will be need it

--- src/App.js
+++ src/App.js
@@ -1,44 +1,39 @@
-import React from "react";
-import { Router } from "@reach/router";
+import React, { useContext } from "react";
+import { Router, Redirect } from "@reach/router";
 import { GlobalStyle } from "./styles/GlobalStyles";
+
 import { NotRegisteredUser } from "./pages/NotRegistredUser";
+import { NotFound } from "./pages/NotFound";
 import { Detail } from "./pages/Details";
 import { Favs } from "./pages/Favs";
 import { User } from "./pages/User";
 import { Home } from "./pages/Home";
-import { Logo } from "./components/Logo";
-import { NavBar } from "./components/NavBar";
-import Context from "./Context";
 
-const UserLogged = ({ children }) => {
-  return children({ isAuth: false }); //HARD CODE STATUS
-};
+import { NavBar } from "./components/NavBar";
+import { Logo } from "./components/Logo";
+
+import { Context } from "./Context";
 
 export const App = () => {
+  const { isAuth } = useContext(Context);
+
   return (
     <div>
       <GlobalStyle />
       <Logo />
       <Router>
-        <Home path={"/"} />
-        <Home path={"/pet/:id"} />
-        <Detail path={"/detail/:detailId"} />
+        <NotFound default />
+        <Home path="/" />
+        <Home path="/pet/:categoryId" />
+        <Detail path="/detail/:detailId" />
+        {!isAuth && <NotRegisteredUser path="/login" />}
+        {!isAuth && <Redirect noThrow from="/favs" to="/login" />}
+        {!isAuth && <Redirect noThrow from="/user" to="/login" />}
+        {isAuth && <Redirect noThrow from="/login" to="/" />}
+        <Favs path="/favs" />
+        <User path="/user" />
       </Router>
-      <Context.Consumer>
-        {({ isAuth }) =>
-          isAuth ? (
-            <Router>
-              <Favs path="/favs" />
-              <User path="/user" />
-            </Router>
-          ) : (
-            <Router>
-              <NotRegisteredUser path="/favs" />
-              <NotRegisteredUser path="/user" />
-            </Router>
-          )
-        }
-      </Context.Consumer>
+
       <NavBar />
     </div>
   );
